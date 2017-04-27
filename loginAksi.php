40ce2fc33e3d4bcdc79a7cb6c6c84d2ba1dc2469
Eggy Prayogia A <eprayogia@live.com>
2017-04-27 10:58:54 +0700

    modifikasi
    penambahan popup saat login

--- loginAksi.php
+++ loginAksi.php
@@ -1,19 +1,22 @@
 <?php
-include "lib/fungsi.php";
+include "../lib/fungsi.php";
 
 $u			= isset($_POST['username']) ? $_POST['username'] : NULL;
 $p	 		= isset($_POST['password']) ? $_POST['password'] : NULL;
 
-$QLogin = mysql_query("SELECT * FROM admin WHERE u = '$u' AND p = '$p'");
-$JLogin	= mysql_num_rows($QLogin);
+$QLogin 	= mysql_query("SELECT * FROM master WHERE u = '$u' AND p = '$p'");
+$JLogin		= mysql_num_rows($QLogin);
 
 if ($JLogin != 1) {
 	echo "<div id='gagal'>Login Gagal. Periksa kembali</div>";
 } else {
-	$AUser = mysql_fetch_array($QLogin);
-	$_SESSION['username'] 	= $AUser[1];
+	$AUser				= mysql_fetch_array($QLogin);
+	$_SESSION['umum'] 	= $AUser['u'];
+	?>
+	<script language="JavaScript">alert('LOGIN SUKSES');
+	document.location=('index.php')</script>
 	
-	echo "<div id='sukses'>Login Sukses. <a href='index.php'>Lanjutkan</a></div>";
+	<?php
 }
 
 ?>
