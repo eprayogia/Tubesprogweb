a836513f3892b87d3693a4e027c070b15f9f2ce2
Eggy Prayogia A <eprayogia@live.com>
2017-05-05 20:06:33 +0700

    file ini sudah fix
    sudah fix dan tidak ada perubahan lagi

--- donload.php
+++ donload.php
@@ -1,7 +1,11 @@
 <?php
+//untuk download
+
 $_file_name = $_GET['filename'];
 
 header("Content-Disposition: attachment; filename=$_file_name");
 header("content-type: text/xml" );
 include ("bck/$_file_name");
+
+
 ?>
